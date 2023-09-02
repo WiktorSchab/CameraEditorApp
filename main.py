e1e893998c9a2c9f16e343951a02a7d02561b04b
WiktorSchab <wiktormateuszschab@gmail.com>
2023-09-02 18:21:38 +0200

    Added camera image displaying in window
    Created funcion using opencv2 to capture camera image and transform it using PIL to display it in window

--- main.py
+++ main.py
@@ -5,12 +5,31 @@ import cv2 as cv
 import pyvirtualcam
 from PIL import Image, ImageTk
 
+
+def camera_update():
+    ret, frame = camera.read()
+
+    if ret:
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
+        frame = Image.fromarray(frame)
+        frame = ImageTk.PhotoImage(image=frame)
+
+        # Setting frame as image
+        center.imgtk = frame
+        center.configure(image=frame)
+
+    # Setting refreshing ratio in ms
+    root.after(10, camera_update)
+
+
 root = tk.Tk()
 
 # Configuration of window
 root.geometry('900x600')
 root.title('Camera Editor App')
 root.resizable(False, False)
+
+camera = cv.VideoCapture(0)
 
 # Content will all
 main_frame = tk.Frame(root)
@@ -30,6 +49,9 @@ center.grid(row=0, column=1)
 
 main_frame.pack(fill='x')
 
+
+camera_update()
+
 root.mainloop()
-
-
+# After closing window
+camera.release()
