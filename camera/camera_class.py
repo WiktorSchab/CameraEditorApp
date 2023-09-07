391199067122c2e6136d5f6fcec948e14062a9df
WiktorSchab <wiktormateuszschab@gmail.com>
2023-09-07 08:51:28 +0200

    Added importing settings from json
    Added importing settings for filter for json file. That change can allows to change settings and save then between sessions of using app

--- camera/camera_class.py
+++ camera/camera_class.py
@@ -1,3 +1,4 @@
+import json
 import numpy as np
 import cv2 as cv
 
@@ -9,12 +10,24 @@ class CameraClass:
         :param window_number: number of clicked tab
         """
         self.active_window_number = window_number
-        
         # Default filter is default (0)
         self.active_filter = 0
-
         # Attribute that informs if settings are already displayed
         self.settings_displayed = 0
+
+    @staticmethod
+    def check_settings(*to_return):
+        data_to_return = []
+
+        # Opening setting file
+        with open('settings/settings.json') as file:
+            data = json.load(file)
+
+        # Searching setting file for certain setting and adding it to array that will be returned
+        for setting in to_return:
+            data_to_return.append(data[setting])
+
+        return data_to_return
 
     # Filter functions
     @staticmethod
@@ -29,11 +42,12 @@ class CameraClass:
 
     @staticmethod
     def blur(frame):
-        ksize = (10, 10)
+        ksize = CameraClass.check_settings('blur_ksize')[0]
         blur = cv.blur(frame, ksize)
         return np.uint8(blur)
 
     @staticmethod
     def bilateral(frame):
-        Bilateral = cv.bilateralFilter(frame, 9, 75, 75)
+        setting = CameraClass.check_settings('bilateral_filter')[0]
+        Bilateral = cv.bilateralFilter(frame, setting[0], setting[1], setting[2])
         return np.uint8(Bilateral)
