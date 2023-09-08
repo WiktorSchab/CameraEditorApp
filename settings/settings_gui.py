5fd63e61ccdccdea763181b30ba748567bbd8a9f
WiktorSchab <wiktormateuszschab@gmail.com>
2023-09-08 21:24:03 +0200

    Added bigger controll with slider_constructor
    Added bigger controll with slider_constructor by adding option to controll range of slider

--- settings/settings_gui.py
+++ settings/settings_gui.py
@@ -40,12 +40,17 @@ def exit_settings_funct(setting_frame, camera_instance, main_frame, settings_to_
 
 
 # Function to create slider with label with tex
-def slider_constructor(frame, instance, setting_to_change, setting_dict, text):
+def slider_constructor(surrounding, setting_to_change, text, range_v):
+    # Unpacking list with variables with info about surrounding of slider
+    frame_tk = surrounding[0]
+    instance = surrounding[1]
+    setting_dict = surrounding[2]
+
     slider_value = instance.check_settings(setting_to_change)[0][0]
 
-    blur_slider_text = tk.Label(frame, text=f" {text}: {slider_value}")
-    blur_slider = tk.Scale(frame,
-                           from_=1, to=20, orient="horizontal",
+    blur_slider_text = tk.Label(frame_tk, text=f" {text}: {slider_value}")
+    blur_slider = tk.Scale(frame_tk,
+                           from_=range_v[0], to=range_v[1], orient="horizontal",
                            command=lambda value: update_value(value, blur_slider_text, setting_to_change, setting_dict)
                            )
     # Setting value of slider by using function that check settings.json file
@@ -63,6 +68,8 @@ def filters_setting(root, main_frame, camera_instance):
     # Hiding widgets
     main_frame.pack_forget()
 
+
+
     # Creating frame and exit button
     setting_frame = tk.Frame(root)
     exit_settings = tk.Button(setting_frame, text='Exit settings',
@@ -72,13 +79,16 @@ def filters_setting(root, main_frame, camera_instance):
     # Content of settings
     title = tk.Label(setting_frame, text='Filters Setting')
 
+    # List with info about where slider will be
+    surroundings_for_slider = [setting_frame, camera_instance, settings_to_update]
+
     # Blur Settings
     content_to_pack.extend(
-        slider_constructor(setting_frame, camera_instance, 'blur_ksize', settings_to_update, 'blur strength'))
+        slider_constructor(surroundings_for_slider, 'blur_ksize', 'blur strength', [1, 20]))
 
     # Bilateral Settings
     content_to_pack.extend(
-        slider_constructor(setting_frame, camera_instance, 'bilateral_filter', settings_to_update, 'bilateral strength'))
+        slider_constructor(surroundings_for_slider, 'bilateral_filter', 'bilateral strength', [1, 20]))
 
     if not camera_instance.settings_displayed:
         title.pack()
